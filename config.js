31343756462044e670d521e1b946dafd25d95e6c
Iz_me praveen <Praveensewmina726@gmail.com>
2024-12-29 13:53:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Su5ThSTL#JyDGaY2rEZTuC1yyWzVbxtyHxRWGi2kIYpBiLykZZio", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "DjRFVaDZ#kYWNpFAICsljum3YANVuhY_8dq1sX9nsUaD6MyDoqcQ", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "ture",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "ture",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",    
